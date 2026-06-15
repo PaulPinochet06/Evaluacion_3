46e8e0d4194eb16fa79e0187d92dac57372edf34
PaulPinochet06 <paulpinochet2@gmail.com>
2026-06-15 18:01:16 -0400

    primera edicion

--- src/Tarjetadeproducto.jsx
+++ src/Tarjetadeproducto.jsx
@@ -6,8 +6,10 @@ function Eleccion({onCambio}) {
         <div>
             <h1>Producto: Mochila </h1>
             <img src={Mochila} alt='mochila'/>
+            <p>Precio: 10000 CLP</p>
             <h1>Producto: Tijeras</h1>
             <img src={Tijeras} alt='tijeras' width={300} height={200}/>
+            <p>Precio: 5000 CLP</p>
             <input
              type="text"
              placeholder="Escribe el producto"
